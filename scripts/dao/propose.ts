80e392c825eb04b3a3542b1bb3a989af7f0197f7
raghav-rama <hackerer528@gmail.com>
2024-02-04 18:57:44 +0530

    Add propose script

---
+++ scripts/dao/propose.ts
@@ -0,0 +1,47 @@
+import { ethers, network } from 'hardhat';
+import {
+  MyGovernor,
+  MyGovernorContractAddress,
+  SimpleStorage,
+  SimpleStorageContractAddress,
+  VOTING_DELAY,
+  devChains,
+} from '../../utils/constants';
+import { moveBlocks } from '../../utils/move_blocks';
+
+async function propose() {
+  const governorContract = await ethers.getContractAt(
+    MyGovernor,
+    MyGovernorContractAddress
+  );
+  const simpleStorage = await ethers.getContractAt(
+    SimpleStorage,
+    SimpleStorageContractAddress
+  );
+  const encodedFunctioncCall = simpleStorage.interface.encodeFunctionData(
+    'store',
+    [42]
+  );
+  console.log('Proposing to set x to 42');
+
+  const proposeTx = await governorContract.propose(
+    [SimpleStorageContractAddress],
+    [0],
+    [encodedFunctioncCall],
+    'Set x to 42'
+  );
+  const proposalResponse = await proposeTx.wait(1);
+
+  console.log('proposalResponse');
+  console.log(proposalResponse);
+  if (devChains.includes(network.name)) {
+    moveBlocks(VOTING_DELAY + 1);
+  }
+}
+
+propose()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
