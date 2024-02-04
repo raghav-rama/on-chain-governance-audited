6a2147988781a09f5de774d18a957c3342847057
raghav-rama <hackerer528@gmail.com>
2024-02-04 18:56:44 +0530

    Run custom scripts

---
+++ bin/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ ! "$1" ]; then
+  echo "Network name must be given." 1>&2
+
+  exit 0
+fi
+
+if [ ! "$2" ]; then
+  echo "Contract Name must be given." 1>&2
+
+  exit 0
+fi
+
+pnpm hardhat run --network "$1" scripts/dao/"$2".ts
